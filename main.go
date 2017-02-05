247fdc99f8b9ef08ed6763e0926ef1fc45a7599e
Rafael Chicoli <rafaelchicoli@hotmail.com>
2017-02-05 22:50:43 +0100

    organize endpoints

--- main.go
+++ main.go
@@ -7,44 +7,54 @@ import (
 	"os"
 )
 
+const (
+	usage = `
+    Endpoints:
+        /            show this response message
+        /health      check health status
+        /hostname    display hostname`
+
+	cowsay = `
+      _______________________________________
+    /  You are only young once, but you can   \
+    \  stay immature indefinitely.            /
+      ---------------------------------------
+            \   ^__^
+             \  (oo)\_______
+                (__)\       )\/\
+                     ||----w |
+                     ||     ||`
+)
+
+func endpoints(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s", usage)
+	log.Printf("%s", r.URL.Path)
+}
+
 func hostname(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
-	fmt.Fprintf(w, "Hostname: %s!", hostname)
-	log.Printf("/: %s", hostname)
-
+	fmt.Fprintf(w, "Hostname: %s", hostname)
+	log.Printf("%s", r.URL.Path)
 }
 
 func cow(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "OK")
-	log.Println("/health: OK")
-	fmt.Fprintf(w, "%v", `
- _________________________________________
-/ You are only young once, but you can    \
-\ stay immature indefinitely.             /
- -----------------------------------------
-   \
-    \
-        .--.
-       |o_o |
-       |:_/ |
-      //   \ \
-     (|     | )
-    /\_   _/\
-    \___)=(___/
-	`)
+	fmt.Fprintf(w, "%s", cowsay)
+	log.Printf("%s", r.URL.Path)
+
 }
 
-func healthcheck(w http.ResponseWriter, r *http.Request) {
+func health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "OK")
-	log.Println("/health: OK")
+	log.Printf("%s", r.URL.Path)
 }
 
 func main() {
-	http.HandleFunc("/", hostname)
 
+	http.HandleFunc("/", endpoints)
 	http.HandleFunc("/cow", cow)
-
-	http.HandleFunc("/health", healthcheck)
+	http.HandleFunc("/health", health)
+	http.HandleFunc("/hostname", hostname)
 
 	http.ListenAndServe(":8080", nil)
+
 }
