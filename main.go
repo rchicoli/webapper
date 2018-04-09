9f120715ecc9befc07b543fd57d45089dd8ff500
rafael_chicoli <rafael_chicoli@regis24.de>
2018-04-09 15:15:11 +0200

    add rabbitmq consume

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/streadway/amqp"
@@ -89,6 +90,26 @@ func (msg *Log) logMessage(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 
+	fmt.Fprintf(w, "%q", msg.Payload.Message)
+	fmt.Printf("%q", msg.Payload.Message)
+
+}
+
+func (msg *Log) rabbitPublish(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// b, err := ioutil.ReadAll(r.Body)
+	// err = json.Unmarshal(b, &msg.Payload)
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&msg.Payload)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		http.Error(w, "error: reading request body", http.StatusInternalServerError)
+	}
+
+	defer r.Body.Close()
+
 	q, err := msg.ch.QueueDeclare(
 		msg.Payload.Queue, // name
 		false,             // durable
@@ -117,6 +138,52 @@ func (msg *Log) logMessage(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%q", msg.Payload.Message)
 }
 
+func (msg *Log) rabbitConsume(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	msg.Payload.Queue = params["queue"]
+
+	q, err := msg.ch.QueueDeclare(
+		msg.Payload.Queue, // name
+		false,             // durable
+		false,             // delete when unused
+		false,             // exclusive
+		false,             // no-wait
+		nil,               // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := msg.ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	loop := true
+	ticker := time.NewTicker(5 * time.Second)
+
+	var bodies []byte
+	bodies = make([]byte, 0)
+
+	for loop {
+		select {
+		case d := <-msgs:
+			bodies = append(bodies, d.Body...)
+			fmt.Printf("%q", d.Body)
+		case <-ticker.C:
+			fmt.Printf("%q", "timeout")
+			loop = false
+		}
+	}
+	fmt.Fprintf(w, "Received a message: %s", string(bodies))
+
+}
+
 var re = regexp.MustCompile(`(\r\n\s+$)|(\r\n$)|(\n\s+$)|(\n$)`)
 
 func main() {
@@ -140,16 +207,16 @@ func main() {
 	router.HandleFunc("/health", healthCheckHandler).Methods("GET")
 	router.HandleFunc("/hostname", hostnameHandler).Methods("GET")
 	router.HandleFunc("/log", m.logMessage).Methods("POST")
+	router.HandleFunc("/rabbit/publish", m.rabbitPublish).Methods("POST")
+	router.HandleFunc("/rabbit/consume/{queue}", m.rabbitConsume).Methods("GET")
 
 	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d/", *username, *password, *host, *port))
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	m.ch, err = conn.Channel()
-	failOnError(err, "Failed to open a channel")
 	defer m.ch.Close()
-
-	failOnError(err, "Failed to publish a message")
+	failOnError(err, "Failed to open a channel")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
